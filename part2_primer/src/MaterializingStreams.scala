b88ad6344486e087a71d343dcca7d54657e22da9
tp86 <tom0palka+tp86@gmail.com>
2021-06-23 20:25:54 +0000

    Add Materializing Streams exercise solution

--- part2_primer/src/MaterializingStreams.scala
+++ part2_primer/src/MaterializingStreams.scala
@@ -41,4 +41,40 @@ object MaterializingStreams extends App {
   //val value = Sink.foreach[Int](println).runWith(Source.single(42)) // NotUsed
   // both ways
   //Flow[Int].map(_ * 2).runWith(simpleSource, simpleSink)
+
+  /** In as many options as possible
+    * - return the last element out of a source (use Sink.last)
+    * - compute the total count of words out of a stream of sentences
+    *   - map, fold, reduce
+    */
+
+  val f1 = Source(1 to 10).toMat(Sink.last)(Keep.right).run()
+  val f2 = Source(1 to 10).runWith(Sink.last)
+
+  val sentenceSource = Source(
+    List(
+      "Akka is awesome",
+      "I love streams",
+      "Materialized values are killing me"
+    )
+  )
+  val wordCountSink = Sink.fold[Int, String](0)((currentWords, newSentence) =>
+    currentWords + newSentence.split(" ").length
+  )
+  val g1 = sentenceSource.toMat(wordCountSink)(Keep.right).run()
+  val g2 = sentenceSource.runWith(wordCountSink)
+  val g3 = sentenceSource.runFold(0)((currentWords, newSentence) =>
+    currentWords + newSentence.split(" ").length
+  )
+
+  val wordCountFlow = Flow[String].fold[Int](0)((currentWords, newSentence) =>
+    currentWords + newSentence.split(" ").length
+  )
+  val g4 = sentenceSource.via(wordCountFlow).toMat(Sink.head)(Keep.right).run()
+  val g5 = sentenceSource
+    .viaMat(wordCountFlow)(Keep.left)
+    .toMat(Sink.head)(Keep.right)
+    .run()
+  val g6 = sentenceSource.via(wordCountFlow).runWith(Sink.head)
+  val g7 = wordCountFlow.runWith(sentenceSource, Sink.head)._2
 }
