d04944291b2072eb325a32e1196f573e3683c7e0
tp86 <tom0palka+tp86@gmail.com>
2021-06-24 13:29:08 +0000

    Operator fusion lecture updates

--- part2_primer/src/OperatorFusion.scala
+++ part2_primer/src/OperatorFusion.scala
@@ -11,7 +11,10 @@ object OperatorFusion extends App {
   val simpleFlow2 = Flow[Int].map(_ * 10)
   val simpleSink = Sink.foreach[Int](println)
 
-  // this runs on the SAME ACTOR (single CPU core) - this is called operator fusion
+  // this runs on the SAME ACTOR (single CPU core)
+  // this is called operator fusion
+  // this is Akka Stream default behavior and
+  //     is good if operations are not time consuming
   simpleSource.via(simpleFlow1).via(simpleFlow2).to(simpleSink)
 
   // "equivalent" behavior
@@ -25,6 +28,39 @@ object OperatorFusion extends App {
     }
   }
   val simpleActor = system.actorOf(Props[SimpleActor]())
-  (1 to 1000).foreach(simpleActor ! _)
+  // (1 to 1000).foreach(simpleActor ! _)
 
+  // complex flows
+  val complexFlow1 = Flow[Int].map { x =>
+    // simulating a long computation
+    Thread.sleep(1000)
+    x + 1
+  }
+  val complexFlow2 = Flow[Int].map { x =>
+    // simulating a long computation
+    Thread.sleep(1000)
+    x * 10
+  }
+  val complexFlowsGraph =
+    simpleSource.via(complexFlow1).via(complexFlow2).to(simpleSink)
+  // complexFlowsGraph.run() // 2 seconds pass between elements being printed
+
+  // async boundary
+  val asyncBoundariesGraph = simpleSource
+    .via(complexFlow1)
+    .async // flow up to this point runs on one actor
+    .via(complexFlow2)
+    .async // flow between async calls runs on separate actor
+    .to(simpleSink) // this runs on yet another actor
+  // asyncBoundariesGraph.run()
+
+  // ordering guarantees
+  Source(1 to 3)
+    .map(element => { println(s"Flow A: $element"); element })
+    .async
+    .map(element => { println(s"Flow B: $element"); element })
+    .async
+    .map(element => { println(s"Flow C: $element"); element })
+    .async
+    .runWith(Sink.ignore)
 }
