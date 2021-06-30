9c86b9d93a4eb6903fa2b53728880e5358b3ea94
tp86 <tom0palka+tp86@gmail.com>
2021-06-30 13:57:28 +0000

    Graph cycles lecture with own fibonacci solution

---
+++ part3_graphs/src/GraphCycles.scala
@@ -0,0 +1,130 @@
+package part3_graphs
+
+import akka.actor.ActorSystem
+import akka.stream.scaladsl.{
+  Broadcast, Flow, GraphDSL, Merge, MergePreferred, RunnableGraph, Sink, Source,
+  ZipWith,
+}
+import akka.stream.{ClosedShape, OverflowStrategy, UniformFanInShape}
+
+object GraphCycles extends App {
+  implicit val system = ActorSystem("GraphCycles")
+
+  val accelerator = GraphDSL.create() { implicit builder =>
+    import GraphDSL.Implicits._
+
+    val sourceShape = builder.add(Source(1 to 100))
+    val mergeShape = builder.add(Merge[Int](2))
+    val incrementerShape = builder.add(Flow[Int].map { x =>
+      println(s"Accelerating $x")
+      x + 1
+    })
+
+    // format: off
+    sourceShape ~> mergeShape ~> incrementerShape
+                   mergeShape <~ incrementerShape
+    // format: on
+
+    ClosedShape
+  }
+
+  RunnableGraph
+    .fromGraph(accelerator)
+  // .run() // prints only first one, then components are quickly backpressured
+  // graph cycle deadlock
+
+  /** Solution 1: MergePreferred */
+  val actualAccelerator = GraphDSL.create() { implicit builder =>
+    import GraphDSL.Implicits._
+
+    val sourceShape = builder.add(Source(1 to 100))
+    val mergeShape = builder.add(MergePreferred[Int](1))
+    val incrementerShape = builder.add(Flow[Int].map { x =>
+      println(s"Accelerating $x")
+      x + 1
+    })
+
+    // format: off
+    sourceShape ~> mergeShape ~> incrementerShape
+                   mergeShape.preferred <~ incrementerShape
+    // format: on
+
+    ClosedShape
+  }
+
+  RunnableGraph
+    .fromGraph(actualAccelerator)
+  // .run()
+
+  /** Solution 2: buffers
+    */
+  val bufferedRepeater = GraphDSL.create() { implicit builder =>
+    import GraphDSL.Implicits._
+
+    val sourceShape = builder.add(Source(1 to 100))
+    val mergeShape = builder.add(Merge[Int](2))
+    val repeaterShape =
+      builder.add(Flow[Int].buffer(10, OverflowStrategy.dropHead).map { x =>
+        println(s"Accelerating $x")
+        Thread.sleep(100)
+        x
+      })
+
+    // format: off
+    sourceShape ~> mergeShape ~> repeaterShape
+                   mergeShape <~ repeaterShape
+    // format: on
+
+    ClosedShape
+  }
+
+  // RunnableGraph.fromGraph(bufferedRepeater).run()
+
+  /** cycles risk deadlocking
+    * - add bounds to the number of elements in the cycle
+    *
+    * boundedness vs liveness (capacity of the graph to not deadlock)
+    */
+
+  /** Challenge: create a fan-in shape
+    * - two inputs which will be fed with EXACTLY ONE number (1 and 1)
+    * - output will emit an INFINITE SEQUENCE based off those 2 numbers
+    * 1, 2, 3, 5, 8, ...
+    *
+    * Hint: use ZipWith and cycles, MergePreferred
+    */
+
+  val fibonacciStaticGraph = GraphDSL.create() { implicit builder =>
+    import GraphDSL.Implicits._
+
+    val merge1 = builder.add(MergePreferred[Int](1))
+    val merge2 = builder.add(MergePreferred[Int](1))
+    val inputBroadcast = builder.add(Broadcast[Int](2))
+    val outputBroadcast = builder.add(Broadcast[Int](2))
+    val adder = builder.add(ZipWith[Int, Int, Int](_ + _))
+
+    merge1 ~> adder.in0
+    merge2 ~> inputBroadcast ~> adder.in1
+    merge1.preferred <~ inputBroadcast
+    adder.out ~> outputBroadcast
+    merge2.preferred <~ outputBroadcast
+
+    UniformFanInShape(outputBroadcast.out(1), merge1.in(0), merge2.in(0))
+  }
+
+  val source1 = Source.single(1)
+  val source2 = Source.single(1)
+  val fibonacciGraph = RunnableGraph.fromGraph(
+    GraphDSL.create() { implicit builder =>
+      import GraphDSL.Implicits._
+
+      val fibonacci = builder.add(fibonacciStaticGraph)
+      source1       ~> fibonacci.in(0)
+      source2       ~> fibonacci.in(1)
+      fibonacci.out ~> Sink.foreach[Int](println)
+
+      ClosedShape
+    },
+  )
+  fibonacciGraph.run()
+}
