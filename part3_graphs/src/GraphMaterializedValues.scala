aec6c33dbfbb8f620a62f8ed746de83a51c6663c
tp86 <tom0palka+tp86@gmail.com>
2021-06-30 09:55:06 +0000

    Materialized values from graphs part2

--- part3_graphs/src/GraphMaterializedValues.scala
+++ part3_graphs/src/GraphMaterializedValues.scala
@@ -1,9 +1,10 @@
 package part3_graphs
 
 import akka.actor.ActorSystem
-import akka.stream.SinkShape
 import akka.stream.scaladsl.{Broadcast, Flow, GraphDSL, Keep, Sink, Source}
+import akka.stream.{FlowShape, SinkShape}
 
+import scala.concurrent.Future
 import scala.util.{Failure, Success}
 
 object GraphMaterializedValues extends App {
@@ -36,13 +37,46 @@ object GraphMaterializedValues extends App {
     },
   )
 
+  import system.dispatcher
   val shortWordsCountFuture =
     wordSource.toMat(complexWordSink)(Keep.right).run()
-  import system.dispatcher
   shortWordsCountFuture.onComplete {
     case Success(count) =>
       println(s"The total number of short words is: $count")
     case Failure(_) =>
       println("Counting the total number of short words failed")
+  }
+
+  /** Exercise: modify flow to return count of elements that passed through it in its materialized value
+    */
+  def enhanceFlow[A, B](flow: Flow[A, B, _]): Flow[A, B, Future[Int]] = {
+    val counterSink = Sink.fold[Int, A](0)((count, _) => count + 1)
+    Flow.fromGraph(
+      GraphDSL.create(counterSink) { implicit builder => sinkShape =>
+        import GraphDSL.Implicits._
+
+        val broadcast = builder.add(Broadcast[A](2))
+        val flowShape = builder.add(flow)
+
+        broadcast ~> sinkShape
+        broadcast ~> flowShape
+
+        FlowShape(broadcast.in, flowShape.out)
+      },
+    )
+  }
+
+  val simpleFlow = Flow[Int].map(identity)
+  val simpleSource = Source(1 to 42)
+  val simpleSink = Sink.ignore
+
+  val enhancedFlowCountFuture =
+    simpleSource
+      .viaMat(enhanceFlow(simpleFlow))(Keep.right)
+      .to(simpleSink)
+      .run()
+  enhancedFlowCountFuture.onComplete {
+    case Success(count) => println(s"$count elements went through flow")
+    case _ => println("Something failed")
   }
 }
