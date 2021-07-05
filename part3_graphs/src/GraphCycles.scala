2e3a1958fa9ce5edcf2d7fc7d9b2f86369d821be
tp86 <tom0palka+tp86@gmail.com>
2021-07-05 10:56:55 +0000

    Fix types for fibonacci solution + official solution

--- part3_graphs/src/GraphCycles.scala
+++ part3_graphs/src/GraphCycles.scala
@@ -3,7 +3,7 @@ package part3_graphs
 import akka.actor.ActorSystem
 import akka.stream.scaladsl.{
   Broadcast, Flow, GraphDSL, Merge, MergePreferred, RunnableGraph, Sink, Source,
-  ZipWith,
+  Zip, ZipWith,
 }
 import akka.stream.{ClosedShape, OverflowStrategy, UniformFanInShape}
 
@@ -97,11 +97,11 @@ object GraphCycles extends App {
   val fibonacciStaticGraph = GraphDSL.create() { implicit builder =>
     import GraphDSL.Implicits._
 
-    val merge1 = builder.add(MergePreferred[Int](1))
-    val merge2 = builder.add(MergePreferred[Int](1))
-    val inputBroadcast = builder.add(Broadcast[Int](2))
-    val outputBroadcast = builder.add(Broadcast[Int](2))
-    val adder = builder.add(ZipWith[Int, Int, Int](_ + _))
+    val merge1 = builder.add(MergePreferred[BigInt](1))
+    val merge2 = builder.add(MergePreferred[BigInt](1))
+    val inputBroadcast = builder.add(Broadcast[BigInt](2))
+    val outputBroadcast = builder.add(Broadcast[BigInt](2))
+    val adder = builder.add(ZipWith[BigInt, BigInt, BigInt](_ + _))
 
     merge1 ~> adder.in0
     merge2 ~> inputBroadcast ~> adder.in1
@@ -112,8 +112,8 @@ object GraphCycles extends App {
     UniformFanInShape(outputBroadcast.out(1), merge1.in(0), merge2.in(0))
   }
 
-  val source1 = Source.single(1)
-  val source2 = Source.single(1)
+  val source1 = Source.single[BigInt](1)
+  val source2 = Source.single[BigInt](1)
   val fibonacciGraph = RunnableGraph.fromGraph(
     GraphDSL.create() { implicit builder =>
       import GraphDSL.Implicits._
@@ -121,10 +121,50 @@ object GraphCycles extends App {
       val fibonacci = builder.add(fibonacciStaticGraph)
       source1       ~> fibonacci.in(0)
       source2       ~> fibonacci.in(1)
-      fibonacci.out ~> Sink.foreach[Int](println)
+      fibonacci.out ~> Sink.foreach[BigInt](println)
 
       ClosedShape
     },
   )
   fibonacciGraph.run()
+
+  // "offical" solution
+  val fibonacciGenerator = GraphDSL.create() { implicit builder =>
+    import GraphDSL.Implicits._
+
+    val zip = builder.add(Zip[BigInt, BigInt]())
+    val mergePreferred = builder.add(MergePreferred[(BigInt, BigInt)](1))
+    val fiboLogic = builder.add(Flow[(BigInt, BigInt)].map {
+      case (last, previous) =>
+        (last + previous, last)
+    })
+    val broadcast = builder.add(Broadcast[(BigInt, BigInt)](2))
+    val extractLast = builder.add(Flow[(BigInt, BigInt)].map(_._1))
+
+    // format: off
+    zip.out ~> mergePreferred ~> fiboLogic ~> broadcast ~> extractLast
+               mergePreferred.preferred    <~ broadcast
+    // format: on
+
+    UniformFanInShape(extractLast.out, zip.in0, zip.in1)
+  }
+
+  val fiboGraph = RunnableGraph.fromGraph(
+    GraphDSL.create() { implicit builder =>
+      import GraphDSL.Implicits._
+
+      val source1 = builder.add(Source.single[BigInt](1))
+      val source2 = builder.add(Source.single[BigInt](1))
+      val sink = builder.add(Sink.foreach[BigInt](println))
+      val fibo = builder.add(fibonacciGenerator)
+
+      source1  ~> fibo.in(0)
+      source2  ~> fibo.in(1)
+      fibo.out ~> sink
+
+      ClosedShape
+    },
+  )
+
+  // fiboGraph.run()
 }
